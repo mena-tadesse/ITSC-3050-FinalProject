576a89de3926b1cb1bd5b5e5d83a7a253a008285
mena-tadesse <menagtadesse@gmail.com>
2025-04-01 23:32:31 -0400

    Initial Commit

---
+++ capstoneTests/capstoneTests.swift
@@ -0,0 +1,17 @@
+//
+//  capstoneTests.swift
+//  capstoneTests
+//
+//  Created by Mena Tadesse on 4/1/25.
+//
+
+import Testing
+@testable import capstone
+
+struct capstoneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
